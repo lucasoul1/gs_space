b9b91239bf2a049c21c9288a2b320b6e711c243e
Lucas <lucasoul.gamedev@gmail.com>
2026-05-26 10:45:55 -0300

    Initial commit

---
+++ Assets/Scripts/Nave.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Nave : MonoBehaviour
+{
+    // Atributos
+    [SerializeField] private float velocidade = 3f;
+    public static int vidas = 3;
+    [SerializeField] private float forcaPulo = 5f;
+    
+    // Movimentação
+    private float mover_x;
+
+    // Propriedades
+    private Rigidbody2D rb;
+    private bool estaNoChao;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = new Vector2(0f, -3.835f);
+    }
+
+    void Update()
+    {
+        // Movimentação
+        mover_x = Input.GetAxis("Horizontal") * velocidade * Time.deltaTime;
+        transform.Translate(mover_x, 0f, 0f);
+
+        // Pulo 
+        if (estaNoChao && Input.GetButtonDown("Jump"))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, forcaPulo);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verifica se está no chão
+        if (collision.gameObject.tag == "Chao" || collision.gameObject.tag == "Plataforma" || collision.gameObject.tag == "PlataformaSome")
+        {
+            estaNoChao = true;
+        }
+
+        // Colisão com Armadilhas
+        if (collision.gameObject.tag == "Serra")
+        {
+            vidas--;
+            print(vidas);
+            SceneManager.LoadScene("GameScene");
+        }
+
+
+        // Sumir Plataforma
+        if (collision.gameObject.tag == "PlataformaSome")
+        {
+            Destroy(collision.gameObject, 2f);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Verifica se saiu do chão
+        if (collision.gameObject.tag == "Chao" || collision.gameObject.tag == "Plataforma")
+        {
+            estaNoChao = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Serra.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Serra : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float velocidade = 2f; 
+    
+    void Start()
+    {
+        transform.position = new Vector2(0f,2f);
+    }
+
+    void Update()
+    {
+        transform.Translate(velocidade * Time.deltaTime, 0f, 0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Parede")
+        {
+            // A serra inverte a direção;
+            velocidade *= -1;
+        }
+    }
+}
