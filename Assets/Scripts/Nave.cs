8614fc4afd44b15397c8065b090c0f78743de9f7
Lucas <lucasoul.gamedev@gmail.com>
2026-06-01 11:14:21 -0300

    add version 0.11

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -3,6 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class Nave : MonoBehaviour
 {
+    private float velocidade;
+    float posicaoInicialX;
+    float posicaoInicialY;
+
+    void Start()
+    {
+        // Propriedas Iniciais
+        velocidade = 0.3f;
+        posicaoInicialX = transform.position.x;
+        posicaoInicialY = transform.position.y;
+    }
+
+    void Update()
+    {
+        movimentacaoVertical();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -21,5 +37,18 @@ public class Nave : MonoBehaviour
     {
         int cenaAtual = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(cenaAtual + 1);
+    }
+
+    void movimentacaoVertical()
+    {
+        float limitePositivo = posicaoInicialY + 0.2f;
+        float limiteNegativo = posicaoInicialY - 0.2f;
+
+        transform.Translate(0f, velocidade * Time.deltaTime, 0f);
+
+        if (transform.position.y >= limitePositivo || transform.position.y <= limiteNegativo)
+        {
+            velocidade *= -1;
+        }
     }
 }
