0a9e69b4700349ede07adf70baa5e56050fce9d0
ACGC <95646819+AdriCGC@users.noreply.github.com>
2026-06-05 14:59:43 -0300

    1.12.2
    Dado Inicio na criação das fases

---
+++ Assets/Scripts/Inimigos/CorrenteDeAsteroides.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CorrenteDeAsteroides : MonoBehaviour
+{
+
+public GameObject Asteroides;
+
+public float IntervaloDeSpawn;
+public float SurgimentoHorizontal;
+
+public int ContadorDeAsteroides;
+public float Espacamento;
+
+}
