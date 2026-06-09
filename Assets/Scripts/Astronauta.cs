4330163b90c5550e092ca6f8d166d7d12572d171
Lucas Ferreira <lucasoul.gamedev@gmail.com>
2026-06-08 23:57:01 -0300

    0.15.4

--- Assets/Scripts/Astronauta.cs
+++ Assets/Scripts/Astronauta.cs
@@ -6,6 +6,7 @@ public class Astronauta : MonoBehaviour
     // Atributos
     [SerializeField] private float velocidade = 3f;
     [SerializeField] private float forcaPulo = 6f;
+    [SerializeField] private Animator animacao;
 
     // Movimenta��o
     private float mover_x;
@@ -26,6 +27,16 @@ public class Astronauta : MonoBehaviour
         // Movimentação
         mover_x = Input.GetAxis("Horizontal") * velocidade * Time.deltaTime;
         transform.Translate(mover_x, 0f, 0f);
+
+        //Animação
+        if (mover_x != 0)
+        {
+            animacao.SetBool("TaAndando", true);
+        }
+        else
+        {
+            animacao.SetBool("TaAndando", false);
+        }
 
         // Pulo
         if (estaNoChao && Input.GetButtonDown("Jump"))
